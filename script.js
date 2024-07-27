9e753089d37d1cfae5d49747524416e6777f5dba
Raphael Gomes <raphagomes494@gmail.com>
2024-07-27 20:58:22 -0300

    gomes-cafe implementando js

---
+++ script.js
@@ -0,0 +1,4 @@
+var n1 = parseFloat(window.prompt("Digite o primeiro numero"))
+var n2 = parseFloat(window.prompt("digite outro numero"))2
+var s = n1 + n2
+window.alert("a soma dos numero é " + s )
