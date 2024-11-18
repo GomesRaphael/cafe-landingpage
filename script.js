3fd5556a71a346891da3448c94b4cceab0c67072
Raphael Gomes <raphagomes494@gmail.com>
2024-11-18 17:29:39 -0300

    Adição do Swiper

--- script.js
+++ script.js
@@ -1,4 +1,57 @@
-var n1 = parseFloat(window.prompt("Digite o primeiro numero"))
-var n2 = parseFloat(window.prompt("digite outro numero"))2
-var s = n1 + n2
-window.alert("a soma dos numero é " + s )
+
+const slideProdutos = new Swiper('.swiperProdutos', {
+  // Optional parameters
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  // If we need pagination
+  // pagination: {
+  //     el: '.swiper-pagination',
+  // },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //     el: '.swiper-scrollbar',
+  // },
+  slidesPerView: 3,
+  spaceBetween: 50,
+  centeredSlides: true,
+  centeredSlidesBounds: true,
+  effect: "coverflow"
+});
+
+var paginatioBlo = new Swiper(".swiperBlog", {
+  grabCursor: true,
+  loop: true,
+  // pagination: {
+  //   el: ".swiper-pagination",
+  // },
+  spaceBetween: 50,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+const buttonReadme = document.querySelector(".xique-button")
+const modal = document.querySelector("dialog")
+const buttonCloseModal = document.querySelector("dialog button")
+
+
+buttonReadme.addEventListener("click", (e) => {
+  modal.show()
+  console.log(e)
+});
+
+buttonCloseModal.addEventListener("click", (e) => {
+  console.log(e)
+  modal.close()
+});
